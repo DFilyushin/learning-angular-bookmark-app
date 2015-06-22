34255ca522a363dcca3d074549d03b88979397e3
Kostya Mikhailyuk <skivsoft@gmail.com>
2015-06-22 13:33:01 +0600

    alt, ctrl and shift modifiers for shortcut component

--- app/component/shortcut/shortcut.js
+++ app/component/shortcut/shortcut.js
@@ -6,19 +6,26 @@
     return {
       restrict: 'E',
       scope: {
+        'altKey': '@',
+        'ctrlKey': '@',
+        'shiftKey': '@',
         'keyCode': '@',
         'keyIdentifier': '@',
         'do': '@'
       },
       link: function ($scope, element, attrs){
         angular.element(document).on('keydown', function(e){
-          if ($scope.keyCode === undefined && $scope.do === undefined) {
+          if ($scope.do === undefined) {
             var info = { altKey: e.altKey, ctrlKey: e.ctrlKey, shiftKey: e.shiftKey, keyCode: e.keyCode, keyIdentifier: e.keyIdentifier };
             console.log('shortcut: ' + JSON.stringify(info));
+            return;
           }
-
-          if ($scope.keyIdentifier !== undefined && e.keyIdentifier == $scope.keyIdentifier ||
-              $scope.keyCode !== undefined && e.keyCode == $scope.keyCode) {
+          var alt = e.altKey === ($scope.altKey !== undefined);
+          var ctrl = e.ctrlKey === ($scope.ctrlKey !== undefined);
+          var shift = e.shiftKey === ($scope.shiftKey !== undefined);
+          if (alt && ctrl && shift &&
+              ($scope.keyIdentifier !== undefined && e.keyIdentifier == $scope.keyIdentifier ||
+               $scope.keyCode !== undefined && e.keyCode == $scope.keyCode)) {
             $scope.$parent.$eval($scope.do);
             $scope.$parent.$digest();
             e.preventDefault();
